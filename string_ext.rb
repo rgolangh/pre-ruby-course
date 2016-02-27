4bb039df8e168e484da291eafa26911ec7674739
Roy Golan <rgolan@redhat.com>
2016-02-28 01:17:46 +0200

    solution
    Signed-off-by: Roy Golan <rgolan@redhat.com>

--- string_ext.rb
+++ string_ext.rb
@@ -1,2 +1,13 @@
 class StringExt
+  def initialize(a , b)
+    @strings = [a, b]
+  end
+
+  def concat()
+    @strings[0] + @strings[1]
+  end
+
+  def substract()
+    @strings[0].gsub(@strings[1], "")
+  end
 end
